bb6c6e1dda846ea18dcaff423283716eba73bc46
Onowoni <danielonowoni1@gmail.com>
2022-07-22 21:36:41 +0100

    reactjs first time learning

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import logo from './logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+      <UnorderedList />
+      <Footer />
+    </>
   );
 }
 
+function Header() {
+  return (
+    <header>
+      <nav><p>img</p></nav>
+    </header>
+  )
+}
+
+function Footer() {
+  return (<footer>2022 Badtnigga development. All rights reserved</footer>)
+}
+
+function UnorderedList() {
+  return (
+    <>
+    <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originallly created by Jordan Walke</li>
+        <li>Has well over 100k stars in Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including Mobile apps</li>
+      </ul>
+    </>
+  )
+}
+
+console.log('red')
+
 export default App;
+
